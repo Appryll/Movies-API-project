3127ce59eedd782c15d4e3cfdeb3c064f6590c43
Natalia FABIANO <natalia.fabiano@hotmail.com>
2023-08-20 02:38:58 +0200

    filters

--- movies/filters.py
+++ movies/filters.py
@@ -3,28 +3,12 @@ from .models import Movies
 from django.db.models import Q
 
 class MoviesFilter(django_filters.FilterSet):
-    movie_filter = django_filters.CharFilter(method='filter_movie')
+    genres = django_filters.CharFilter(field_name='genres', lookup_expr='icontains')
 
-    """Filter for Books by Price"""
-    vote_average = django_filters.RangeFilter()
-
+    before = django_filters.DateFilter(field_name='release_date', lookup_expr= 'gte', label='before')
+    after = django_filters.DateFilter(field_name='release_date', lookup_expr='lte', label='after')
     class Meta:
         model = Movies
-        fields = ['vote_average', ]
-        # fields = {
-        #     'genres': ['icontains'],
-        #     'vote_average' : ['lte', 'gte'],
-        #     'before' : movie_filter
-
-        # }
-        #fields = ['genre', 'release_date__lte', 'movie_filter']
-        
-    # def filter_movie(self, qs, name, value):
-    #     return qs.filter(
-    #         Q(client_id__last_name__icontains=value) | Q(client_id__email__icontains=value)
-    #     )
-
-# qs = Movies.objects.all()
-
-# # Range: vote_average between 0 and 10
-# f = F({'vote_average_min': '0', 'vote_average_max': '10'}, queryset=qs)
+        fields = {
+            'vote_average' : ['range',]
+        }
